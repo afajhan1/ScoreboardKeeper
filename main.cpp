c42e67530410168bf4093f875aeab23905a776ea
afajhan1 <afajhan@mail.csuchico.edu>
2018-04-12 22:25:17 -0700

    last step

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -7,6 +9,10 @@ const int MAX_PERIODS = 10;
 const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
+vector< vector<int> > Scoreboard;
+
+void printScoreboard(vector < vector <int> >);
+int randomBetween(int,int);
 
 int main()
 {
@@ -27,9 +33,76 @@ int main()
   }
   else
   {
+   
    //make scoreboard and fill it with zeros
   
-   //once created, display the scoreboard
+  Scoreboard.resize(teams);
+  for(int row = 0; row < Scoreboard.size(); row++)
+  {
+      Scoreboard[row].resize(periods);
+
+      for(int col = 0; col < Scoreboard[row].size(); col++)
+      {
+           Scoreboard[row][col] = 0;
+      }
+
+   }
+
+   /* cout<<"SCOREBOARD"<<endl;
+  
+  for(int r = 0; r < teams; r++)
+   {
+   cout<<Player "<<r + 1<<": ";|
+
+   for(int c = 0; c < periods; c++)
+   {
+   cout<<Scoreboard[r][c]<<"|";
+
+   }
+  
+  cout<<endl;
+  }*/
+  
+  printScoreboard(Scoreboard);
+  for(int row = 0; row < Scoreboard.size(); row++)
+  {
+     for(int col = 0; col < Scoreboard[row].size(); col++)
+     {
+         Scoreboard[row][col] = randomBetween(5,5);
+     }
+   }
+   printScoreboard(Scoreboard);
+
+  //once created, display the scoreboard
   }
   return 0;
 }
+void printScoreboard(vector < vector <int> > board)
+{
+      cout<<"SCOREBOARD"<<endl;
+
+      for(int r = 0; r < board.size(); r++)
+      {
+           cout<<"Player "<<r + 1<<": ";
+
+           for(int c = 0; c < board[r].size(); c++)
+           {
+                 cout<<board[r][c]<<"|";
+
+                 }
+                 cout<<endl;
+                 }
+
+     }
+     int randomBetween(int first, int second)
+     {
+     if(first > second)
+     {
+     return second + rand()%(first-second+1);
+     }
+     else if( second > first)
+     {
+     return first + rand()%(second-first+1);
+     }
+     }
+
